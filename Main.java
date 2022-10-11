055fe097a3f7ab43e66d2e4da6313ae784cc7752
BoiSad <92589705+BoiSad@users.noreply.github.com>
2022-10-11 09:43:44 +0200

    Add files via upload
    11.10

---
+++ Main.java
@@ -0,0 +1,95 @@
+import java.util.Scanner;
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        //zad1
+        System.out.println(getMyName());
+        System.out.println(getMyAge());
+        //zad2
+        getMyNumbers(2, 5);
+        //zad3
+        Parzysta(2);
+
+    }
+
+    //metody
+    public static String getMyName() {
+        return "Jan";
+    }
+
+    public static int getMyAge() {
+        return 23;
+    }
+
+    public static void getMyNumbers(int l1, int l2) {
+        System.out.println(l1 + " + " + l2 + " = " + (l1 + l2));
+        System.out.println(l1 + " - " + l2 + " = " + (l1 - l2));
+        System.out.println(l1 + " * " + l2 + " = " + (l1 * l2));
+    }
+
+    public static boolean Parzysta(int a) {
+        Scanner in = new Scanner(System.in);
+
+        if (a % 2 == 0) {
+            System.out.println("parzysta");
+            return true;
+
+        }return false;
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+        /*
+
+       String imie,miasto;
+       int wiek;
+
+       // wczytywanie danych z klawiatury
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Podaj wiek: ");
+        wiek = input.nextInt();
+        System.out.println("Podaj Imie: ");
+        imie = input.next();
+        System.out.println("Podaj Miasto: ");
+        miasto = input.next();
+
+        System.out.println("imie: "+imie+", miasto: "+miasto+ ", wiek: "+wiek);
+
+
+
+/*
+
+
+
+
+
+        /* System.out.println("Hello world!");
+        System.out.println("Witaj");
+        int a,b;
+        float c = 2.9F;
+        double d = 3.3443;
+        boolean pytanie = true;
+        String Imie = "Jan", miasto = "Rzeszow";
+        int wiek = 24;
+            System.out.println("imie: "+Imie+" miasto: "+miasto+ ", wiek: "+wiek); /*
+
+         */
+
+
+
+
